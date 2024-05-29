34d87f25d50aa85c6782284452b2058c3a4d71a5
HARINIM2405 <149925922+HARINIM2405@users.noreply.github.com>
2024-05-29 16:04:10 +0530

    Add files via upload

---
+++ DIGITAL CLOCK.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("CLOCK")
+
+def time():
+    string = strftime(' %H:%M:%S %p  ')
+    label.config(text=string)
+    label.after(1000, time)
+
+frame = Frame(root, borderwidth=5, relief="solid", padding=(10, 10))
+frame.pack(padx=20, pady=20, anchor='center')
+
+label = Label(frame, font=("ds-digital", 80), background="grey", foreground="black")
+label.pack()
+
+time()
+mainloop()
